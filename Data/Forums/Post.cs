bccde3790192feedbe7c0d4506671ae63f35d38c
JEres89 <jens.eresund@gmail.com>
2022-07-08 00:27:24 +0200

    ForumController actions placeholders finished(?)

--- Data/Forums/Post.cs
+++ Data/Forums/Post.cs
@@ -1,13 +1,24 @@
-﻿namespace Community_BackEnd.Data.Forums;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Community_BackEnd.Data.Forums;
 
 public class Post
 {
+	[Key]
+	[ReadOnly(true)]
 	public int Id { get; set; }
+	[Required]
 	public string Author { get; set; }
+	[Required, ReadOnly(true)]
 	public DateTime TimeStamp { get; set; }
 	public DateTime TimeStampEdit { get; set; }
 	public int ReplyTo { get; set; }
+	[Required]
+	public Topic Topic { get; set; }
+	[Required]
 	public string Content { get; set; }
+
 	public override string ToString()
 	{
 		return $"{Id},{Author},{TimeStamp},{TimeStampEdit},{ReplyTo},{{{Content}}}";
