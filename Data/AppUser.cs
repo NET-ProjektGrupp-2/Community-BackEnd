d7d934cbdd08371c018c4f6c422c84bd538cfcd6
JEres89 <jens.eresund@gmail.com>
2022-07-11 12:27:49 +0200

    Matched actual data classes with documentation

--- Data/AppUser.cs
+++ Data/AppUser.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Community_BackEnd.Data.Forums;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Community_BackEnd.Data;
 
 public class AppUser : IdentityUser
 {
+	public string DisplayName { get; set; }
+	[Required(AllowEmptyStrings = false)]
+	public string Firstname { get; set; }
+	[Required(AllowEmptyStrings = false)]
+	public string Surname { get; set; }
+	public bool HidePersonalInfo { get; set; } = true;
+	[Required]
+	public DateTime CreationDate { get; set; }
+	public Dictionary<string,string> AboutMe { get; set; }
+	public List<Topic> Topics { get; set; }
+	public List<Post> Posts { get; set; }
 
 }
--- Data/Forums/Forum.cs
+++ Data/Forums/Forum.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using Microsoft.AspNetCore.Identity;
 
 namespace Community_BackEnd.Data.Forums;
 
@@ -8,14 +9,17 @@ public class Forum
 	[Key]
 	[ReadOnly(true)]
 	public int Id { get; set; }
-	[Required]
+	[Required(AllowEmptyStrings = false)]
 	public string Name { get; set; }
-	[Required]
+	[Required(AllowEmptyStrings = false)]
 	public string Description { get; set; }
 
 	public Forum? ParentForum { get; set; }
 	public List<Forum> SubForums { get; set; } = new List<Forum>();
 	public List<Topic> Topics { get; set; } = new List<Topic>();
+
+	public List<AppUser> Moderators { get; set; }
+	public IdentityRole? Restricted { get; set; }
 
 	public override string ToString()
 	{
