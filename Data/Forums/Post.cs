390319eb05c670669f583b83a3b76e7f9eff07a5
JEres89 <jens.eresund@gmail.com>
2022-07-13 12:41:47 +0200

    EF and SQL server both finally accepts the configuration

--- Data/Forums/Post.cs
+++ Data/Forums/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Community_BackEnd.Data.Forums;
 
@@ -11,12 +12,15 @@ public class Post
 	[Required, ReadOnly(true)]
 	public DateTime PostDate { get; set; } = DateTime.Now;
 	public DateTime? EditDate { get; set; }
-	[Required]
-	public string AuthorId { get; set; }
+	[BindRequired]
+	public string? AuthorId { get; set; }
+	public AppUser? Author { get; set; }
 	public int? ContextPostId { get; set; }
+	public Post? ContextPost { get; set; }
+	public List<Post> Replies { get; set; }
 	[Required]
 	public int TopicId { get; set; }
-
+	public Topic Topic { get; set; }
 	[Required(AllowEmptyStrings =false)]
 	public string Content { get; set; }
 
--- Data/NewsArticle.cs
+++ Data/NewsArticle.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using Community_BackEnd.Data.Forums;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Community_BackEnd.Data;
@@ -11,17 +13,21 @@ public class NewsArticle
 	[Required(AllowEmptyStrings = false)]
 	public string Title { get; set; }
 
-	[Required, ReadOnly(true)]
-	public string AuthorId { get; set; }
+	[BindRequired, ReadOnly(true)]
+	public string? AuthorId { get; set; }
+	public AppUser? Author { get; set; }
 
 	[Required(AllowEmptyStrings = false)]
 	public string Content { get; set; }
-	public int DiscussionThreadId { get; set; }
-	public List<string> Categories { get; set; }
+	[BindRequired]
+	public int? TopicId { get; set; }
+	public Topic? Topic { get; set; }
+	//Json object with a category names array
+	public string Categories { get; set; } = @"{ Categories: ['News','Site Development'] }";
 
 	[Required, ReadOnly(true)]
 	public DateTime PublishDate { get; set; } = DateTime.Now;
-	public DateTime EditDate { get; set; }
+	public DateTime? EditDate { get; set; }
 
 	//public override string ToString()
 	//{
